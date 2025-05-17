cc0e4babc9bbd3aaa573ffe999886ebb5e5ec603
Miguel Sanchez-Grice <msanchezgrice@gmail.com>
2025-05-17 00:50:10 -0500

    Fix redirect to generator page after login

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -7,7 +7,7 @@ export const dynamic = 'force-dynamic';
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
-  const next = requestUrl.searchParams.get('next') || '/'; // Get optional next redirect
+  const next = requestUrl.searchParams.get('next') || '/generator'; // Get optional next redirect
 
   if (code) {
     const supabase = createClient(); // Use our server client
